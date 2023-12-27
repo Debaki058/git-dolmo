f75cdeccb45ba2badbbb17e343f2d469f131bd03
Debaki058 <debakiblon30@gmail.com>
2023-12-27 16:09:39 +0545

    first commit

---
+++ DebakiApp1/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using
+    System;
+
+namespace DebakiApp1
+{
+    internal class Vehicle
+    {
+        public string brand = "Fold";
+        public void honk()
+        {
+            Console.WriteLine("Tuut ,tuut");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public string modelName = "Mustang";
+    }
+
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Car myCar = new Car();
+            myCar.honk();
+            Console.WriteLine(myCar.brand + " " + myCar.modelName);
+            Console.ReadKey();
+        }
+    }
+}
